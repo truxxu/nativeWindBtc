ae32367a71e2a1894f0a7270afa9d99e76910452
Daniel Trujillo <danielfetrujillo@gmail.com>
2024-03-02 08:27:58 -0500

    created template for screen and text

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import {View, Text, Button} from 'react-native';
+import {Button} from 'react-native';
 
 import {RootStackNavigationProp} from '../types/navigation';
+import {ScreenTemplate} from '../organisms';
+import {Text} from '../atoms';
 
 type Props = {
   navigation: RootStackNavigationProp<'Home'>;
@@ -9,13 +11,14 @@ type Props = {
 
 const Home: React.FC<Props> = ({navigation}) => {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text>Home</Text>
+    <ScreenTemplate>
+      <Text text="Welcome" variant="title" />
+      <Text text="Select a pair to see more details" />
       <Button
         title="Go to Details"
         onPress={() => navigation.navigate('Details')}
       />
-    </View>
+    </ScreenTemplate>
   );
 };
 

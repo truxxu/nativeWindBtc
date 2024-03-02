fced9a3252c8311316d287ec9b4e650f6337fad1
Daniel Trujillo <danielfetrujillo@gmail.com>
2024-03-02 07:16:51 -0500

    created navigation stack

--- App.tsx
+++ App.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-import {Home} from './src/screens';
+import {Home, Pair} from './src/screens';
+
+const Stack = createNativeStackNavigator();
 
 const App: React.FC = () => {
-  return <Home />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen name="Pair" component={Pair} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default App;
